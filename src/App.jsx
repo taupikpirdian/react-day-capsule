da243a1e5cf30333ae1bc9b98135859eaf316f11
Taupik Pirdian <taupik.pirdia@salt.co.id>
2024-09-25 21:51:03 +0700

    initiate vite

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+      <div className="flex justify-center items-center h-screen bg-blue-100">
+        <div>
+          Day Capsule
+        </div>
+      </div>
+  )
+}
+
+export default App
