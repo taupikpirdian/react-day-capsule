599dc01844e3b2e23e2c3fc08c8c690958cb516e
Taupik Pirdian <taupik.pirdia@salt.co.id>
2024-09-27 16:09:17 +0700

    feat: [login][tauik] page login

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 function App() {
   return (
-    <div className="flex justify-center items-center h-screen bg-[#111827] p-0">
-      <div className="flex w-full h-full">
+    <div className="flex justify-center items-center p-0">
         <div className="w-full justify-center items-center p-12">
-          <div className="w-full h-full bg-[#1F2937] rounded-xl p-10">
-            
+          <div className="w-full h-full bg-[#1F2937] rounded-xl p-10 h-auto">
             <div class="grid grid-cols-1">
-              <div>         
+              <div className="flex items-center">         
                 <img className="img-logo" src="assets/images/logo/logo.png" alt="logo" />
-                <p className="text-white text-xl font-bold">Day Capsule</p>
-                <p className="f-weight-light">track, reflect, improve</p>
+                <div className="flex flex-col ml-3">
+                  <p className="text-white text-xl font-bold">Day Capsule</p>
+                  <p className="f-weight-light">track, reflect, improve</p>
+                </div>
               </div>
             </div>
 
@@ -21,19 +21,26 @@ function App() {
             <form>
                 <div class="grid gap-6 mb-6 md:grid-cols-2">
                   <div>
-                      <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                      <input type="email" className="bg-gray-50 border border-gray-300 rounded-lg p-2 bg-gray-100 focus:outline-none focus:ring-2 focus:ring-[#F6A417] dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="Email" required/>
+                      <label for="first_name" class="label">Email</label>
+                      <input type="email" className="form-control" placeholder="Email" required/>
                   </div>
                   <div>
-                      <label for="last_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                      <input type="password" className="bg-gray-50 border border-gray-300 rounded-lg p-2 bg-gray-100 focus:outline-none focus:ring-2 focus:ring-[#F6A417] dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="Password" required/>
+                      <label for="last_name" class="label">Password</label>
+                      <input type="password" className="form-control" placeholder="Password" required/>
                   </div>
                 </div>
+
+                <div class="inline-flex items-center justify-center w-full">
+                    <hr class="w-64 h-px my-8 bg-gray-200 border-0 dark:bg-gray-700 mr-2" />
+                    <span className="text-white">or</span>
+                    <hr class="w-64 h-px my-8 bg-gray-200 border-0 dark:bg-gray-700 ml-2" />
+                </div>
+                
                 <div class="flex items-start mb-6">
                     <div class="flex items-center h-5">
-                    <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required />
+                      <input id="remember" type="checkbox" value="" class="fc-checkbox" required />
                     </div>
-                    <label for="remember" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
+                    <label for="remember" class="ms-2 label">Remember me</label>
                 </div>
                 <button 
                     type="submit" 
@@ -44,10 +51,9 @@ function App() {
             </form>
           </div>
         </div>
-        <div className="w-full flex justify-center items-center">
+        <div className="w-full flex justify-center items-center hidden sm:flex pt-[3%]">
           <img src="assets/images/login/think.svg" alt="think" />
         </div>
-      </div>
     </div>
   )
 }
